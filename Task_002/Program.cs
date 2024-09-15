c4f510d719dfd1ebf4171c7c9265a472093c465d
MARY <zlllodeika@yandex.ru>
2024-09-15 19:04:36 +0300

    Добавлено решение Task 01-03

---
+++ Task_002/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости,
+// в которой находится эта точка.
+
+Console.WriteLine("Введите координату X: ");
+int XNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y: ");
+int YNumber = Convert.ToInt32(Console.ReadLine());
+
+if (XNumber >0 && YNumber > 0) 
+{
+    System.Console.WriteLine($"{XNumber}, {YNumber} => 1 четверть");
+}
+else if (XNumber < 0 && YNumber > 0) 
+{
+    System.Console.WriteLine($"{XNumber}, {YNumber} => 2 четверть");
+}
+else if (XNumber < 0 && YNumber < 0) 
+{
+    System.Console.WriteLine($"{XNumber}, {YNumber} => 3 четверть");
+}
+else
+{
+    System.Console.WriteLine($"{XNumber}, {YNumber} => 4 четверть");
+}
+
---
+++ Task_003/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход целое число
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Console.Write("Введите число из отрезка [10, 99]: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstNumber = number / 10;
+int secondNumber = number % 10;
+int maxNumber = firstNumber > secondNumber ? firstNumber : secondNumber;
+Console.WriteLine(maxNumber);
